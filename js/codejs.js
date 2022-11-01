e86dcdd62336ffbfd8c54d443aaa3c4b53dcfb8d
tungtv <tungtv@vnpt.vn>
2022-11-01 22:56:16 +0700

    push code 01112022 23h56p

---
+++ js/codejs.js
@@ -0,0 +1,11 @@
+//document.getElementById("example1").innerHTML = "Hello JavaScript!";
+/*var a = 5;
+var b;
+b = "6";
+var c = a + b;
+
+alert(c);*/
+function $thu_xem() {
+alert("ok");
+}
+$thu_xem();
